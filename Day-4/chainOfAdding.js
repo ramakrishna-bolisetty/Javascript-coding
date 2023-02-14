68734489b6b286c2c9bbcd52346e56f5cbdb7e15
ramakrishna-bolisetty <124112871+ramakrishna-bolisetty@users.noreply.github.com>
2023-02-14 15:19:10 +0530

    Add files via upload

---
+++ Day-4/chainOfAdding.js
@@ -0,0 +1,25 @@
+// DESCRIPTION:
+// We want to create a function that will add numbers together when called in succession.
+
+// add(1)(2); // == 3
+// We also want to be able to continue to add numbers to our chain.
+
+// add(1)(2)(3); // == 6
+// add(1)(2)(3)(4); //  == 10
+// add(1)(2)(3)(4)(5); // == 15
+// var addTwo = add(2);
+// addTwo; // == 2
+// addTwo + 5; // == 7
+// addTwo(3); // == 5
+// addTwo(3)(5); // == 10
+
+function add(n){
+      // Let the curryig begin!
+        function resultFn(y){
+            return add(n+y);
+        }
+        resultFn.valueOf = function(){
+                return n;
+            };
+        return resultFn;
+    }
