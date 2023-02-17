6b1d17a952d7a854aaf86ca418d7e324997512d5
ramakrishna-bolisetty <124112871+ramakrishna-bolisetty@users.noreply.github.com>
2023-02-17 10:45:47 +0530

    Update functionComposition-2.js

--- Day-4/functionComposition-2.js
+++ Day-4/functionComposition-2.js
@@ -17,3 +17,15 @@
 // A simple implementation of compose, could work as follows:
 
 // const compose = (f, g) => (a) => f(g(a))
+
+const addOne = (a) => a + 1
+const multTwo = (b) => b * 2
+
+function compose(...args) {
+    // Your solution
+    return function(n){
+      args.reverse().forEach((i)=>n=i(n));
+      return n;
+    }
+  }
+  console.log(compose(multTwo,addOne)(5));
