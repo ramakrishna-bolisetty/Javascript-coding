1d3ed6876dcb85b261665a2b284e7bd9a325f37d
ramakrishna-bolisetty <124112871+ramakrishna-bolisetty@users.noreply.github.com>
2023-02-20 19:08:51 +0530

    Update functionComposition-1.js

--- Day-4/functionComposition-1.js
+++ Day-4/functionComposition-1.js
@@ -1,22 +1,26 @@
-// Function composition is a mathematical operation that mainly presents 
-// itself in lambda calculus and computability. It is explained well here, 
-// but this is my explanation, in simple mathematical notation:
+/*Function composition is a mathematical operation that mainly presents 
+itself in lambda calculus and computability. It is explained well here, 
+but this is my explanation, in simple mathematical notation:
 
-// f3 = compose( f1 f2 )
-//    Is equivalent to...
-// f3(a) = f1( f2( a ) )
-// Your task is to create a compose function to carry out this task,
-//  which will be passed two functions or lambdas. Ruby functions will be passed,
-//   and should return, either a proc or a lambda. Remember that the resulting composed function may 
-//   be passed multiple arguments!
+f3 = compose( f1 f2 )
+   Is equivalent to...
+f3(a) = f1( f2( a ) )
+Your task is to create a compose function to carry out this task,
+ which will be passed two functions or lambdas. Ruby functions will be passed,
+  and should return, either a proc or a lambda. Remember that the resulting composed function may 
+  be passed multiple arguments!
 
-// compose(f , g)(x)
-// => f( g( x ) )
-// This kata is not available in haskell; that would be too easy!
+compose(f , g)(x)
+=> f( g( x ) )
+This kata is not available in haskell; that would be too easy!
+*/
 
-function compose(f,g) {
-    // Compose the two functions here!
-     return function() {
-      return f(g.apply(this,arguments));
-    }
+const add = (a) => a + 1;
+const mult = (b) => b * 2;
+const  compose=function(f, g) {
+  return function(){
+    return f(g.apply(this, arguments));
   }
+}
+const result = compose(mult,add)(3);
+console.log(result); // 8 
