fc1635a3639802052f4794c6e48dcdd60d2c1bf5
ramakrishna-bolisetty <124112871+ramakrishna-bolisetty@users.noreply.github.com>
2023-02-20 18:50:05 +0530

    Update chainOfAdding.js

--- Day-4/chainOfAdding.js
+++ Day-4/chainOfAdding.js
@@ -1,31 +1,31 @@
-// DESCRIPTION:
-// We want to create a function that will add numbers together when called in succession.
+/*We want to create a function that will add numbers together when called in succession.
 
-// add(1)(2); // == 3
-// We also want to be able to continue to add numbers to our chain.
+add(1)(2); // == 3
+We also want to be able to continue to add numbers to our chain.
 
-// add(1)(2)(3); // == 6
-// add(1)(2)(3)(4); //  == 10
-// add(1)(2)(3)(4)(5); // == 15
-// var addTwo = add(2);
-// addTwo; // == 2
-// addTwo + 5; // == 7
-// addTwo(3); // == 5
-// addTwo(3)(5); // == 10
+add(1)(2)(3); // == 6
+add(1)(2)(3)(4); //  == 10
+add(1)(2)(3)(4)(5); // == 15
+var addTwo = add(2);
+addTwo; // == 2
+addTwo + 5; // == 7
+addTwo(3); // == 5
+addTwo(3)(5); // == 10
+*/
 
-function add(n) {
-    // Let the curryig begin!
-  
-    function resultFn(y) {
-        if(y!=undefined)
-        return add(n + y);
-        // else return n;
+const addition = (n) => {
+    const innerFun = (y) => {
+        if (y != undefined)
+            return addition(n + y);
+        else
+            return n;
     }
-    resultFn.valueOf = function () {
+    innerFun.valueOf =  ()=> {
         return n;
     };
-    return resultFn;
+    return innerFun;
 }
 
-console.log(add(add(1)(2)(3))(3).valueOf());
+const result=addition(1)(2)(3);
+console.log(result.valueOf()); // 6
 
